5dfa39e5d11bb9e7a7c2c3c5d96bb5e354c700fe
Nícolas Maduro <nicolasamaduro@gmail.com>
2018-05-17 22:58:07 -0300

    tudo pronto

--- server/app.js
+++ server/app.js
@@ -1,33 +1,43 @@
-var express = require('express'),
-    app = express();
+const express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    _ = require('underscore');
 
-// carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
-// você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
-// dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
-var db = {
+let db = {
 };
 
+app.set('view engine', 'hbs');
+app.set('views',`${__dirname}/views`)
 
-// configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???');
+db.jogadores =JSON.parse(fs.readFileSync(`${__dirname}/data/jogadores.json`, "utf8"));
+db.jogosPorJogador =JSON.parse(fs.readFileSync(`${__dirname}/data/jogosPorJogador.json`, "utf8"));
+app.get('/', function (req, res) {
+  res.render('index',db.jogadores);
+})
 
+app.get('/jogador/:numero_identificador', function (req, res) {
+ let numero_identificador= req.params.numero_identificador
+ let jogosDesteJogador = db.jogosPorJogador[numero_identificador].games;
+  let naoJogados = _.where(jogosDesteJogador,
+    { playtime_forever: 0 });
+  let ordenadoDesc = _.sortBy(jogosDesteJogador, function(jogo) {
+    return -jogo.playtime_forever;
+  });
+  var primeiros5 = _.first(ordenadoDesc, 5);
+  let jogador = _.find(db.jogadores.players, function(j) {
+    return j.steamid===numero_identificador;
+    });
+    primeiros5.map(jogo=>  jogo.playtime_forever = (jogo.playtime_forever/60).toFixed(0));
+  res.render('jogador',{
+    jogador:jogador,
+    naoJogados: naoJogados,
+    primeiros5: primeiros5,
+    jogosDesteJogador:ordenadoDesc
+  });
+})
 
-// EXERCÍCIO 2
-// definir rota para página inicial --> renderizar a view index, usando os
-// dados do banco de dados "data/jogadores.json" com a lista de jogadores
-// dica: o handler desta função é bem simples - basta passar para o template
-//       os dados do arquivo data/jogadores.json
+app.use(express.static(`${__dirname}/../client`));
 
-// EXERCÍCIO 3
-// definir rota para página de detalhes de um jogador --> renderizar a view
-// jogador, usando os dados do banco de dados "data/jogadores.json" e
-// "data/jogosPorJogador.json", assim como alguns campos calculados
-// dica: o handler desta função pode chegar a ter umas 15 linhas de código
-
-
-// EXERCÍCIO 1
-// configurar para servir os arquivos estáticos da pasta "client"
-// dica: 1 linha de código
-
-// abrir servidor na porta 3000
-// dica: 1-3 linhas de código
+const server = app.listen(3000, () => {
+  console.log('Escutando em: http://localhost:3000');
+});
